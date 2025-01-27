c42f4cba01ce8aa067c642167f236ee2a68eaa95
Manas Jain Kuniya <138317061+this-is-mjk@users.noreply.github.com>
2025-01-28 01:54:13 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/rozReturns", // <=== repo name
+  // basePath: "/rozReturns", // <=== repo name
   output: "export", // <=== enables static exports
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
